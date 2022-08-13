e5f8e072fa974be73ca5b0f201325b4d192a41fc
matsuyami <mkadwaki@gmail.com>
2022-08-12 21:19:05 -0700

    added graph

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-var bars = document.querySelectorAll('.bar');
-var days = document.querySelectorAll('.day')
-const currDay = new Date().getDay();
-console.log(currDay);
+const col = document.querySelectorAll('.col')
+const dayLabels = document.querySelectorAll('.day')
+const bars = document.querySelectorAll('.bar')
+const currDay = new Date().getDay()
 
-// days[0].textContent 
+const dayToNum = {
+  'sun': 0, 
+  'mon': 1,
+  'tue': 2,
+  'wed': 3,
+  'thu': 4,
+  'fri': 5,
+  'sat': 6,
+}
+
+async function showGraph(){
+  const resp = await fetch('./data.json')
+  const json = await resp.json()
+  const days = json.map(data => data.day)
+  const barHeights = json.map(data => data.amount)
+
+  for (const [index, day] of days.entries()) { 
+    let text = document.createTextNode(day)
+    bars[index].style.height = `${barHeights[index] * 2.5}px`
+    col[index].appendChild(bars[index])
+    dayLabels[index].appendChild(text)
+    col[index].appendChild(dayLabels[index])
+
+    if(dayToNum[day] === currDay % 7) { 
+      bars[index].classList += ' today '
+    } 
+  }
+}
+
+
+showGraph()
+
